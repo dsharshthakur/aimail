aea205886741147d70dc19b7cbb7ce898b928075
HARSH SINGH <131766995+dsharshthakur@users.noreply.github.com>
2023-12-17 01:04:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ if generate_btn == True:
         with st.container(border = True ) as response_container:
             
             st.markdown("<h5>Response:</h5>", unsafe_allow_html=True)
-            st.markdown(
+            response_container.markdown(
     """
     <style>
         .st-dl {
